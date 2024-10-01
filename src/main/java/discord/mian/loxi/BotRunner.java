4c9d38b225b6b33aab8b6d353eae9f9cbad9271b
LoxiGoose <docgoosenn@gmail.com>
2024-10-01 12:17:28 -0700

    Jar can now properly be built

--- src/main/java/discord/mian/loxi/BotRunner.java
+++ src/main/java/discord/mian/loxi/BotRunner.java
@@ -3,14 +3,9 @@ package discord.mian.loxi;
 import discord.mian.loxi.common.Bot;
 import discord.mian.loxi.common.Listener;
 import discord.mian.loxi.common.util.Constants;
-import io.github.sashirestela.openai.SimpleOpenAI;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.AnnotatedEventManager;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 // TODO: Remake all features from the original ai typescript branch into java branch. I LOVE JAVA :D
 public class BotRunner {
